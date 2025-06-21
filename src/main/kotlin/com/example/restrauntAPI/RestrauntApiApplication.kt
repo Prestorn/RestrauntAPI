455eee6ecfb1795e110f7233917e7855a8cf0d6f
Alexander Yurkov <yr@example.com>
2025-06-21 19:38:42 +0300

    init

---
+++ src/main/kotlin/com/example/restrauntAPI/RestrauntApiApplication.kt
@@ -0,0 +1,11 @@
+package com.example.restrauntAPI
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RestrauntApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<RestrauntApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/restrauntAPI/RestrauntApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.restrauntAPI
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RestrauntApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
